34de9cf45bfaf3f20be79c10fce2cfb247282fe2
devethan <devohno.ko@gmail.com>
2020-05-26 21:57:01 +0900

    Remove test-code

--- src/components/__tests__/ToggleSwitch.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import {render, fireEvent, waitFor, screen} from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
-import ToggleSwitch from '../ToggleSwitch';
-
-test('render test', () => {
-  render(<ToggleSwitch />);
-});
